4c09ebd03fb4cd0049e4b276888bb889cb4ee6dd
Thanh Truong <hopchuathurac@gmail.com>
2019-04-24 00:09:07 +0200

    Slim Maven dependencies for pubsub client

---
+++ src/test/java/org/apache/beam/examples/PublisherExample.java
@@ -0,0 +1,59 @@
+package org.apache.beam.examples;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutures;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.ProjectTopicName;
+import com.google.pubsub.v1.PubsubMessage;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PublisherExample {
+
+    // use the default project id
+    private static final String PROJECT_ID = "bbr-segmentation-services";
+
+    @Test
+    public void test() throws Exception {
+        // topic id, eg. "my-topic"
+        String topicId = "dataflow-pipeline-test";
+        int messageCount = Integer.parseInt("3");
+        ProjectTopicName topicName = ProjectTopicName.of(PROJECT_ID, topicId);
+        Publisher publisher = null;
+        List<ApiFuture<String>> futures = new ArrayList<>();
+
+        try {
+            // Create a publisher instance with default settings bound to the topic
+            publisher = Publisher.newBuilder(topicName).build();
+
+            for (int i = 0; i < messageCount; i++) {
+                String message = "message-" + i;
+
+                // convert message to bytes
+                ByteString data = ByteString.copyFromUtf8(message);
+                PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
+                        .setData(data)
+                        .build();
+
+                // Schedule a message to be published. Messages are automatically batched.
+                ApiFuture<String> future = publisher.publish(pubsubMessage);
+                futures.add(future);
+            }
+        } finally {
+            // Wait on any pending requests
+            List<String> messageIds = ApiFutures.allAsList(futures).get();
+
+            for (String messageId : messageIds) {
+                System.out.println("message " + messageId);
+            }
+
+            if (publisher != null) {
+                // When finished with the publisher, shutdown to free up resources.
+                publisher.shutdown();
+            }
+        }
+    }
+}
